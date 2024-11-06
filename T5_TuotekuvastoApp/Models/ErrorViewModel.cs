71484fc2ece25d066a6ef81d1504bc3baf1aaf35
M1xed <mikokuokkanen@hotmail.com>
2024-11-06 15:49:33 +0200

    Add project files.

---
+++ T5_TuotekuvastoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T5_TuotekuvastoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
